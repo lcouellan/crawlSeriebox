cdb0f1e5756433bd1a5b3f566106b307695b2bba
Lénaïc <lcouellan@gmail.com>
2016-10-04 16:06:08 +0200

    Modif Crawler

--- seriebox/spiders/sao_spider.py
+++ seriebox/spiders/sao_spider.py
@@ -17,5 +17,15 @@ class SaoSpyder(scrapy.Spider):
                 'dureeEp':sao.css('h3::text').extract_first(),
                 'genres': sao.css('h3#item_genre a::text').extract(),
                 'producteur': sao.css('h2 a::text').extract_first(), 
-                'acteurs': sao.css('h2 a::text').extract()            
+                'acteurs': sao.css('h2 a::text').extract(),
+                'pays': sao.css('img::attr(alt)').extract_first()            
             }
+        for sao in response.css('#fiche_v2_header_average'):
+        	yield {
+        		'note': sao.css('span.average_number::text').extract_first(),
+        		'nombreVotes': sao.css('span.nbrevotes_number::text').extract_first()
+        	}
+        for sao in response.css('div#row_manage_serie'):
+        	yield {
+        		'synopsis': sao.css('div#tvshow_synopsis p::text').extract_first()
+        	}
