86d5572267def4706ebc374c982e29e400189501
Lénaïc <lcouellan@gmail.com>
2016-10-04 16:19:22 +0200

    Modif du crawler

--- seriebox/spiders/sao_spider.py
+++ seriebox/spiders/sao_spider.py
@@ -9,23 +9,17 @@ class SaoSpyder(scrapy.Spider):
         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for sao in response.css('div.col-md-6#fiche_v2_header_title'):
+        for sao in response.css('body'):
             yield {
-            	'nom': sao.css('h1::text').extract_first(),
-            	'dateDiffusion': sao.css('p#item_year::text').extract_first(),
-                'statut': sao.css('p#item_year .status_serie::text').extract_first(),
-                'dureeEp':sao.css('h3::text').extract_first(),
-                'genres': sao.css('h3#item_genre a::text').extract(),
-                'producteur': sao.css('h2 a::text').extract_first(), 
-                'acteurs': sao.css('h2 a::text').extract(),
-                'pays': sao.css('img::attr(alt)').extract_first()            
-            }
-        for sao in response.css('#fiche_v2_header_average'):
-        	yield {
-        		'note': sao.css('span.average_number::text').extract_first(),
-        		'nombreVotes': sao.css('span.nbrevotes_number::text').extract_first()
-        	}
-        for sao in response.css('div#row_manage_serie'):
-        	yield {
-        		'synopsis': sao.css('div#tvshow_synopsis p::text').extract_first()
+            	'nom': sao.css('#fiche_v2_header_title h1::text').extract_first(),
+            	'dateDiffusion': sao.css('#fiche_v2_header_title p#item_year::text').extract_first(),
+                'statut': sao.css('#fiche_v2_header_title p#item_year .status_serie::text').extract_first(),
+                'dureeEp':sao.css('#fiche_v2_header_title h3::text').extract_first(),
+                'genres': sao.css('#fiche_v2_header_title h3#item_genre a::text').extract(),
+                'producteur': sao.css('#fiche_v2_header_title h2 a::text').extract_first(), 
+                'acteurs': sao.css('#fiche_v2_header_title h2 a::text').extract(),
+                'pays': sao.css('#fiche_v2_header_title h2 img::attr(alt)').extract_first(),            
+        		'note': sao.css('#fiche_v2_header_average span.average_number::text').extract_first(),
+        		'nombreVotes': sao.css('#fiche_v2_header_average span.nbrevotes_number::text').extract_first(),
+        		'synopsis': sao.css('#row_manage_serie #tvshow_synopsis p::text').extract_first()
         	}
