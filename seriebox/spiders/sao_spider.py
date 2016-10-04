7c2c297bf48f48f681c0664e17ca9e8f35b53a62
Lénaïc <lcouellan@gmail.com>
2016-10-04 15:38:16 +0200

    Modif du crawler

--- seriebox/spiders/sao_spider.py
+++ seriebox/spiders/sao_spider.py
@@ -9,9 +9,13 @@ class SaoSpyder(scrapy.Spider):
         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for sao in response.css('div#fiche_v2_header_title'):
+        for sao in response.css('div.col-md-6#fiche_v2_header_title'):
             yield {
-                'nom': sao.css('h1::text').extract(),
-                'statut': sao.css('p#item_year .status_serie::text').extract(),
-                'synopsis':sao.css('div#tvshow_synopsis p::text').extract()
+            	'nom': sao.css('h1::text').extract_first(),
+            	'dateDiffusion': sao.css('p#item_year::text').extract_first(),
+                'statut': sao.css('p#item_year .status_serie::text').extract_first(),
+                'dureeEp':sao.css('h3::text').extract_first(),
+                'genres': sao.css('h3#item_genre a::text').extract(),
+                'producteur': sao.css('h2 a::text').extract_first(), 
+                'acteurs': sao.css('h2 a::text').extract()            
             }
